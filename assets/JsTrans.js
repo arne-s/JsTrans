381e20119862583091ee91b1711ccc26813315e8
Arne <arne@stulp.net>
2012-08-15 13:36:28 +0200

    refactor, improve comments

--- assets/JsTrans.js
+++ assets/JsTrans.js
@@ -27,37 +27,37 @@ Yii.translate.do = function (message, params, dictionary) {
         // extract number from params
         if (params % 1 === 0) params = {'n':params}; // param is numeric, convert to object key for convenience
         if (params.n % 1 === 0) num = params.n;
-    }
 
-    // split translation into pieces
-    var chunks = translation.split('|');
+        // split translation into pieces
+        var chunks = translation.split('|');
 
-    if (translation.indexOf('#') !== -1) { // translation contains expression
-        for (var i = 0; i < chunks.length; i++) {
-            var pieces = chunks[i].split('#'), // split each chunk in two parts (0: expression, 1: message)
-                ex = pieces[0],
-                msg = pieces[1];
+        if (translation.indexOf('#') !== -1) { // translation contains expression
+            for (var i = 0; i < chunks.length; i++) {
+                var pieces = chunks[i].split('#'), // split each chunk in two parts (0: expression, 1: message)
+                    ex = pieces[0],
+                    msg = pieces[1];
 
-            if (pieces.length == 2) {
-                // handle number shortcut (0 instead of n==0)
-                if (ex % 1 === 0) ex = 'n==' + ex;
+                if (pieces.length == 2) {
+                    // handle number shortcut (0 instead of n==0)
+                    if (ex % 1 === 0) ex = 'n==' + ex;
 
-                // create expression to be evaluated (e.g. n>3)
-                var eval_expr = ex.split('n').join(num);
+                    // create expression to be evaluated (e.g. n>3)
+                    var eval_expr = ex.split('n').join(num);
 
-                // if expression matches, set translation to current chunk
-                if (eval(eval_expr)) {
-                    translation = msg;
-                    break;
+                    // if expression matches, set translation to current chunk
+                    if (eval(eval_expr)) {
+                        translation = msg;
+                        break;
+                    }
                 }
             }
         }
-    }
-    // if translation doesn't contain # but does contain |, treat it as simple choice format
-    else if (chunks.length > 1) translation = (num == 1) ? chunks[0] : chunks[1];
+        // if translation doesn't contain # but does contain |, treat it as simple choice format
+        else if (chunks.length > 1) translation = (num == 1) ? chunks[0] : chunks[1];
 
-    // replace placeholder/replacements
-    if (typeof(params == 'Object')) for (var key in params) translation = translation.split('{' + key + '}').join(params[key]);
+        // replace placeholder/replacements
+        if (typeof(params == 'Object')) for (var key in params) translation = translation.split('{' + key + '}').join(params[key]);
+    }
 
     return translation;
 }
@@ -77,7 +77,7 @@ Yii.t = function (category, message, params, language) {
 
     var dictionary = false;
 
-    //  language not found, return original message
+    // find dictionary
     if (typeof Yii.translate.dictionary[lang] !== 'undefined')
         if (category && typeof Yii.translate.dictionary[lang][category] !== 'undefined')
             dictionary = Yii.translate.dictionary[lang][category];
